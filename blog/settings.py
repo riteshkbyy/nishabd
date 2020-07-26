55dd4c1e839bba992154910660661a4f9ca0a412
ritesh yadav <riteshkbyy@gmail.com>
2020-07-26 09:34:21 +0530

    make it better

--- blog/settings.py
+++ blog/settings.py
@@ -25,7 +25,7 @@ DJANGO_MODE = os.getenv('DJANGO_MODE', "Production").lower()
 SECRET_KEY = '&49ga4rqxrp)^w=%ed&s4qf0*)1yvl!$6j=e@b^^e=i-s3g+@+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['herokuapp.com']
 
